e0c322854561abfb791c6775986fcd024b9f1247
Yashgaur908 <Yashgaur908@gmail.com>
2023-08-09 20:56:42 +0530

    added todo task

---
+++ accordion_app/src/Accordion.js
@@ -0,0 +1,13 @@
+import React, {ueState} from 'react'
+
+const Accordion = () => {
+
+    const [show, setShow] = useState('true')
+    return(
+        <>
+            <div>Accordion</div>
+        </>
+    )
+}
+
+export default Accordion;
---
+++ accordion_app/src/App.js
@@ -0,0 +1,12 @@
+import Accordion from './Accordion.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Accordion />
+    </div>
+  );
+}
+
+export default App;
