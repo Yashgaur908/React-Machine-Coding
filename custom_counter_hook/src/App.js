68b2900f9fd042c845016cc92039b4d705f55a30
Yashgaur908 <Yashgaur908@gmail.com>
2023-08-11 17:58:29 +0530

    created custom_counter_hook

---
+++ custom_counter_hook/src/App.js
@@ -0,0 +1,12 @@
+import CustomHook from './CustomHook.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomHook />
+    </div>
+  );
+}
+
+export default App;
